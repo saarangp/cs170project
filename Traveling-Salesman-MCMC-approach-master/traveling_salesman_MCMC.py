3cebed49d8d1c5ae796954dfeaea6b48976e7799
LesInfos <risheekp@berkeley.edu>
2019-12-06 02:59:58 -0800

    integratable

--- Traveling-Salesman-MCMC-approach-master/traveling_salesman_MCMC.py
+++ Traveling-Salesman-MCMC-approach-master/traveling_salesman_MCMC.py
@@ -11,6 +11,8 @@ import networkx as nx
 import random
 import math
 
+
+
 # MCMC (Simulated Annealing) solution for Traveling Salesman problem
 # start from city 1 to city N
 
@@ -23,25 +25,33 @@ import math
 # distance[ind_diag, ind_diag] = 0
 #print(distance)
 
+'''CHANGE THESE BOIZ RIGHT HERE'''
+ITER0 = 30
+ITER1 = 10000
+ITER2 = 5000
+'''Keep ITER0 relatively low, and ITER1 about two times as much as ITER2
+For reference, 500 : 100000 : 50000 took about 1.5 mintues for a random input of size 200
+while 50 : 10000 : 5000 took about 12 seconds for a random input of size 200
+
+
+I'm guessing that 30 : 10000 : 5000 is optimal for decent time, 
+but feel free to reduce them or increase the last two values if you want for time
+'''
+
+
+'''
+
+MAKE SURE THAT
+the input is a fully connected graph that has weighted edges
+
+returns list of edges that solver will travel to starting and ending with location 0 (which should be soda hall)
+(we can also return the distance if necessary)
+'''
+
 # Calculate total distance for a given sequence
 def mcmc_solver(G):
-    #G = nx.complete_graph(N)
-    for (u, v) in G.edges():
-        G.edges[u, v]['weight'] = random.randint(0, 10)
     distance = nx.to_numpy_matrix(G)
-    print(distance)
-
-
-    #Straight up wrong
-    # def form_tsp(reduced_graph, matrix = True):
-    #     comp_g = nx.complete_graph(len(reduced_graph.nodes))
-    #     for (u, v) in G.edges():
-    #         comp_g.edges[u, v]['weight'] = random.randint(0, 10)
-    #
-    #     if matrix:
-    #         return nx.to_numpy_matrix(comp_g).np.matrix.tolist()  # 2 dimensional adjascency list
-    #     else:
-    #         return comp_g   #nx complete graph with weights
+    #print(distance)
 
 
     def cal_dist(distance, L):
@@ -53,38 +63,34 @@ def mcmc_solver(G):
 
     T = float(pow(2, -8)) # free parameters, inversely related to the probability of rejection if the direction is wrong
 
-    ITER0 = 100
-    ITER1 = 5000
-    ITER2 = 5000
-    L = np.append(np.arange(N), [0]) # initail route sequence
-    #np.insert(L, len(L), 0)
-    print(L)
-    print (cal_dist(distance, L)) # initial distance
-    dist_all = []
 
-    # for i in range(ITER0):
-    #     c = np.random.randint(1, math.ceil((N - 1)/2))
-    #     a = np.random.randint(1, N - 1, size = c)
-    #     b = np.random.randint(1, N - 1, c)
-    #     d_t = cal_dist(distance, L)
-    #     dist_all.append(d_t)
-    #     L_tmp = copy.copy(L)
-    #     for k in c:
-    #         L_tmp[[a, b]] = L_tmp[[b, a]]
-    #     delta_d = cal_dist(distance, L_tmp) - d_t
-    #     p = min(1, np.exp(-1 * delta_d / T))
-    #     u = np.random.rand()
-    #     if u < p:
-    #         L = L_tmp
+    L = np.append(np.arange(N), [0]) # initail route sequence
+    # print(L)
+    # print (cal_dist(distance, L)) # initial distance
+    # dist_all = []
+
+    for i in range(ITER0):
+        c = np.random.choice(math.ceil((N - 1)/2), size = 1, replace=False)
+        a = np.random.randint(1, N - 1, size = c)
+        b = np.random.randint(1, N - 1, size = c)
+        d_t = cal_dist(distance, L)
+        # dist_all.append(d_t)
+        L_tmp = copy.copy(L)
+        for k in range(c[0]):
+            L_tmp[[a[k], b[k]]] = L_tmp[[b[k], a[k]]]
+        delta_d = cal_dist(distance, L_tmp) - d_t
+        p = min(1, np.exp(-1 * delta_d / T))
+        u = np.random.rand()
+        if u < p:
+            L = L_tmp
 
     for i in range(ITER1):
         a = np.random.randint(1, N - 1)
-        #b = (a + 1)%N
         b = np.random.randint(1, N - 1)
         if a == b:
             b = (a + 1)%N
         d_t = cal_dist(distance, L)
-        dist_all.append(d_t)
+        # dist_all.append(d_t)
         L_tmp = copy.copy(L)
         L_tmp[[a, b]] = L_tmp[[b, a]]
         delta_d = cal_dist(distance, L_tmp) - d_t
@@ -96,7 +102,7 @@ def mcmc_solver(G):
         a = np.random.randint(1, N - 1)
         b = (a + 1)%N
         d_t = cal_dist(distance, L)
-        dist_all.append(d_t)
+        # dist_all.append(d_t)
         L_tmp = copy.copy(L)
         L_tmp[[a, b]] = L_tmp[[b, a]]
         delta_d = cal_dist(distance, L_tmp) - d_t
@@ -104,13 +110,22 @@ def mcmc_solver(G):
         u = np.random.rand()
         if u < p:
             L = L_tmp
-    print(L)
-    print (cal_dist(distance, L)) # final distance
-    plt.plot(dist_all)
-    plt.show()
+    # print(list(L))
+    # print (cal_dist(distance, L)) # final distance
+    # plt.plot(dist_all)
+    # plt.show()
+    return(list(L))
+
+# def christo_solver(G):
+#     distance_matrix = list(nx.to_numpy_matrix(G))
+#     TSP = christofides.compute(distance_matrix)
 
 
 
-N = 50
-g = nx.complete_graph(N)
-mcmc_solver(g)
+
+N = 25
+G = nx.complete_graph(N)
+for (u, v) in G.edges():
+    G.edges[u, v]['weight'] = random.randint(0, 10)
+mcmc_solver(G)
+# christo_solver(G)
